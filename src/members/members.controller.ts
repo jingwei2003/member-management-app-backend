6511f928fd939b58de90cf2e0b764505e1eb067d
jingwei2003 <yeyinling2003@gmail.com>
2024-11-25 22:58:36 +0800

    modify async await

--- src/members/members.controller.ts
+++ src/members/members.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { MembersService } from './members.service';
 import { CreateMemberDto } from './dto/create-member.dto';
